1f9772bf6ef20250dfd82b129920709001d05c00
renan tomazini <renantomazini@gmail.com>
2022-07-05 01:15:32 -0300

    Fim da aula 1

---
+++ aula_1/funcaoBuiltIn.R
@@ -0,0 +1,41 @@
+#funcoes built-in
+
+exp(1)
+exp(log(5))
+sin(pi/2)
+max(1,2,3,5,2)
+min(1,2,3,5,2)
+sum(2,1,4,5)
+prod(6,3,1,2)
+sqrt(25)
+factorial(5)
+F == FALSE
+teste_ <-0
+
+sum((1:2)^2)
+
+x <- 136
+y <- 36
+z <- x + y
+res <- log(sqrt(z))
+res <- round(res, 0)
+
+fib <-sum(0,1,1,2,3)
+resp <- round(log(fib,exp(1)),3)
+
+x <- 2
+derivar <-sin(x)+x
+derivada <- D(derivar,"x")
+
+
+fat10 <- factorial(10)
+rest <-round(log(fat10,10),2)
+
+velocidadeLuz <- 300000000
+energia = 10 * (velocidadeLuz)^2
+res = round(log(energia),2)
+
+
+x<-c(2,5,7,8,9)
+pol = x^3+x^2+x
+f <- mean(pol)
--- aula_1/var.R
+++ aula_1/var.R
@@ -1,3 +1,19 @@
 aux <- 10
+
 aux_2 <-15
+
 aux_3 <-5
+
+ls()
+rm(aux_2)
+
+rm(list=ls())
+
+
+hello <-"Olá mundo, aqui é R"
+
+num1 <-5
+num2 <-2
+num3 <- num1/num2
+num3
+rm(list=ls())
