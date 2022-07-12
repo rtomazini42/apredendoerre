737c50e7955f3dda2c40b2c69f9c3e26019d8cb0
renan tomazini <renantomazini@gmail.com>
2022-07-11 22:48:26 -0300

    Exercicios do dia

--- aula2/Exercício 04.R
+++ aula2/Exercício 04.R
@@ -11,11 +11,26 @@ drinks
 nomes=c("João", "Paula", "Maria", "Ingrid", "José", "Marcos")
 pesos=c(80, 65, 70, 58, 78, 70)
 alturas=c(1.70, 1.66, 1.65, 1.60, 1.76, 1.70)
-IMC<-pesos/(alturas*2)
+IMC<-pesos/(alturas*alturas)
 nomes[1]
-lista_pessoas=list(nomes,pesos,alturas,(pesos/(alturas*2))
+lista_pessoas<-list(nomes,pesos,alturas,IMC)
 str(lista_pessoas)
 View(lista_pessoas)
 lista_pessoas[[1]][1]
 table(lista_pessoas)
 lista_pessoas[[4]][3]
+
+
+lista<-lapply(airquality, function(x){mean(x)})
+media<-(lista$Temp)
+
+load("chuvas.RData")
+chuvas
+media<-mean(chuvas)
+sum(chuvas)
+rownames(chuvas)
+max(colMeans(chuvas))
+max(rowMeans(chuvas))
+
+colMeans(chuvas)
+min(colMeans(chuvas))
